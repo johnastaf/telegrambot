96bc0a9eef800352b750b251a3db06856cd6a51d
john <johnastaf@gmail.com>
2019-06-22 11:47:52 +0300

    Add schedule

--- bot.py
+++ bot.py
@@ -1,24 +1,46 @@
 import telebot
 import time
 from telebot import apihelper
+import schedule
+import time
 
 bot = telebot.TeleBot('880475938:AAFZEAOKADR3YIIAa_Tggihlq6FPJVHi00E')  
 
-apihelper.proxy = {'https': 'socks5://50.195.104.171:1080'}
+apihelper.proxy = {'https': 'socks5://5.135.58.124:64980'}
 
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
-    bot.send_message(message.chat.id, 'Hello from bot')
-
+    bot.send_message(message.chat.id, 'schedule is running!')
+    #schedule.every().day.at('11-33').do(timer_alert, message)
+    schedule.every().day.at("12:30").do(timer_alert, message)
+    #schedule.every(1).minutes.do(timer_alert, message)
+    while True:
+        schedule.run_pending()
 
 @bot.message_handler(commands=['go'])
 def go_message(message):
     bot.send_message(message.chat.id, 'Go from bot')
 
+
+@bot.message_handler(content_types=['text'])
+def send_text(message):
+    if message.text.lower() == 'hi':
+        bot.send_message(message.chat.id, 'Hi, John')
+    elif message.text.lower() == 'bye':
+        bot.send_message(message.chat.id, 'Bye, John')
+
+
+def timer_alert(message):
+    #nowTime = datetime.now()
+    bot.send_message(message.chat.id, 'schedule works!')
+    #print ('%s' % nowTime.strftime("%d.%m.%Y %H:%M:%S"), '// Send test timer message')
+
+
 while True:
     try:
-        bot.polling(none_stop=True, timeout=200)
+        print('Starting...')
+        bot.polling(none_stop=True, timeout=1000)
     except Exception as E:
         print(E.args)
         time.sleep(5)
