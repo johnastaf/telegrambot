cec91e8b30363743b41ef666ebd4be33096712bb
john <johnastaf@gmail.com>
2019-07-12 06:26:26 +0300

    Add geo

--- bot.py
+++ bot.py
@@ -3,18 +3,18 @@ import time
 from telebot import apihelper
 import schedule
 import time
+from telebot import types
 
 bot = telebot.TeleBot('880475938:AAFZEAOKADR3YIIAa_Tggihlq6FPJVHi00E')  
 
-apihelper.proxy = {'https': 'socks5://5.135.58.124:64980'}
+apihelper.proxy = {'https': 'socks5://148.72.209.6:15681'}
 
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
     bot.send_message(message.chat.id, 'schedule is running!')
-    #schedule.every().day.at('11-33').do(timer_alert, message)
-    schedule.every().day.at("12:30").do(timer_alert, message)
-    #schedule.every(1).minutes.do(timer_alert, message)
+    #schedule.every().day.at('11:10').do(timer_alert, message)
+    schedule.every(1).minutes.do(timer_alert, message)
     while True:
         schedule.run_pending()
 
@@ -22,11 +22,24 @@ def start_message(message):
 def go_message(message):
     bot.send_message(message.chat.id, 'Go from bot')
 
+@bot.message_handler(commands=["geo"])
+def geo(message):
+    keyboard = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
+    button_geo = types.KeyboardButton(text="Отправить местоположение", request_location=True)
+    keyboard.add(button_geo)
+    bot.send_message(message.chat.id, "ЕЕе! Нажми на кнопку и передай мне свое местоположение", reply_markup=keyboard)
+
+@bot.message_handler(content_types=["location"])
+def location(message):
+    if message.location is not None:
+        print(message.location)
+        print("latitude: %s; longitude: %s" % (message.location.latitude, message.location.longitude))
 
 @bot.message_handler(content_types=['text'])
 def send_text(message):
+    print('{id}: {text}'.format(id=message.from_user.first_name, text=message.text))
     if message.text.lower() == 'hi':
-        bot.send_message(message.chat.id, 'Hi, John')
+        bot.send_message(message.chat.id, 'Hi, John and Misha :-)')
     elif message.text.lower() == 'bye':
         bot.send_message(message.chat.id, 'Bye, John')
 
